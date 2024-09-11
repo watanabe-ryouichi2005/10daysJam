ee59d07150a453c232b1a06167f5dc70975a1543
cho-yohan <k023g1037@g.neec.ac.jp>
2024-09-11 13:07:56 +0900

    タイトルシーンで選択

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -8,10 +8,12 @@
 #include "PrimitiveDrawer.h"
 #include "TextureManager.h"
 #include "TitleScene.h"
+#include "TutorialScene.h"
 #include "WinApp.h"
 
 GameScene* gameScene = nullptr;
 TitleScene* titleScene = nullptr;
+TutorialScene* tutorialScene = nullptr;
 ClearScene* clearScene = nullptr;
 OverScene* overScene = nullptr;
 
@@ -19,6 +21,7 @@ enum class Scene {
 	kUnknown = 0,
 
 	kTitle,
+	kTutorial,
 	kGame,
 	kDead,
 	kClear,
@@ -29,12 +32,25 @@ Scene scene = Scene::kTitle;
 void ChangeScene() {
 	switch (scene) {
 	case Scene::kTitle:
+
 		if (titleScene->IsFinished()) {
 			// シーン変更
-			scene = Scene::kGame;
+			scene = Scene::kTutorial;
 			// 旧シーンの解放
 			delete titleScene;
 			titleScene = nullptr;
+			// 新シーンの生成と初期化
+			tutorialScene = new TutorialScene();
+			tutorialScene->Initialize();
+		}
+		break;		
+	case Scene::kTutorial:
+		if (tutorialScene->IsFinished()) {
+			// シーン変更
+			scene = Scene::kGame;
+			// 旧シーンの解放
+			delete tutorialScene;
+			tutorialScene = nullptr;
 			// 新シーンの生成と初期化
 			gameScene = new GameScene();
 			gameScene->Initialize();
@@ -87,6 +103,9 @@ void UpdateScene() {
 	case Scene::kTitle:
 		titleScene->Update();
 		break;
+	case Scene::kTutorial:
+		tutorialScene->Update();
+		break;
 	case Scene::kGame:
 		gameScene->Update();
 		break;
@@ -102,6 +121,9 @@ void DrawScene() {
 	switch (scene) {
 	case Scene::kTitle:
 		titleScene->Draw();
+		break;
+	case Scene::kTutorial:
+		tutorialScene->Draw();
 		break;
 	case Scene::kGame:
 		gameScene->Draw();
@@ -204,6 +226,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// 各種解放
 	delete titleScene;
+	delete tutorialScene;
 	delete gameScene;
 	delete overScene;
 	delete clearScene;
